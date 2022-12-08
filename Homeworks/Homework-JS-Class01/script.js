2d2c09ea4dd28a39f23433431882899c206d9eb3
Kozoloski <107805572+Kozoloski@users.noreply.github.com>
2022-12-08 15:31:49 +0100

    Delete Homeworks directory

--- Homeworks/Homework-JS-Class01/script.js
+++
@@ -1,7 +0,0 @@
-var number_of_phones =30;
-var phoneprice_without_tax = 119.95;
-var phone_tax = 119.95 * 0.05;
-var phoneprice_with_tax = phoneprice_without_tax + phone_tax;
-var thirtyphonesprice = phoneprice_with_tax * number_of_phones;
-
-console.log(`The price of thirty phones is $${thirtyphonesprice}`);
