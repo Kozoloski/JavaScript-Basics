118810be432dace8480af0929747d0e39ff1dea0
Kozoloski <107805572+Kozoloski@users.noreply.github.com>
2022-12-14 02:31:49 +0100

    Add files via upload

---
+++ Homework-Class04-1/script.js
@@ -0,0 +1,7 @@
+let elements = ["Tomo", "happy", "working"];
+
+function tellstory(){
+    console.log(`This is ${elements[0]}. ${elements[0]} is a nice person. Today he is ${elements[1]}. He is ${elements[2]} all day.`);
+}
+
+tellstory(elements);
